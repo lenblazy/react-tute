4d82ed49e8dcc1f9b2d162dc020a1e0fe75d54d5
Lennox Mwabonje <lenblazy@gmail.com>
2025-01-21 15:17:37 +0300

    Handle state

--- src/components/ClassBasedComponent.jsx
+++ src/components/ClassBasedComponent.jsx
@@ -1,9 +1,37 @@
 import React, {Component} from "react";
 
 class ClassBasedComponent extends Component {
-    render(){
+
+    state = {
+        // write all state properties inside of this
+        count: 0,
+    }
+
+    // constructor(props) {
+    //     super(props);
+    //     this.state = {
+    //         // write all state properties inside of this
+    //         count: 0,
+    //     };
+    //     this.handleClick = this.handleClick.bind(this);
+    // }
+
+    handleClick = () => {
+        const {count} = this.state;
+        this.setState({
+            count: count + 1,
+        }, () => console.log(count));
+    }
+
+    render() {
         return (
-            <div>Hellow ClassBasedComponent</div>
+            <div>
+                Hellow ClassBasedComponent
+                <button onClick={this.handleClick}>Click me</button>
+                {
+                    this.state.count === 5 && "Count is 5"
+                }
+            </div>
         );
     }
 }
