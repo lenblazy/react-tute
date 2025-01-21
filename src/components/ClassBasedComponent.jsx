3696c49f608f08ffad80a59cf79cb093266f4a84
Lennox Mwabonje <lenblazy@gmail.com>
2025-01-21 15:41:11 +0300

    Style css

--- src/components/ClassBasedComponent.jsx
+++ src/components/ClassBasedComponent.jsx
@@ -5,6 +5,7 @@ class ClassBasedComponent extends Component {
     state = {
         // write all state properties inside of this
         count: 0,
+        flag: false
     }
 
     // constructor(props) {
@@ -23,6 +24,24 @@ class ClassBasedComponent extends Component {
         }, () => console.log(count));
     }
 
+    componentDidMount() {
+        console.log('Component is mounted');
+    }
+
+    componentDidUpdate(prevProps, prevState, snapshot) {
+
+        if (prevState && prevState.count !== this.state.count && this.state.count === 10) {
+            this.setState({
+                flag: true
+            })
+        }
+
+    }
+
+    componentWillUnmount() {
+
+    }
+
     render() {
         return (
             <div>
@@ -30,6 +49,9 @@ class ClassBasedComponent extends Component {
                 <button onClick={this.handleClick}>Click me</button>
                 {
                     this.state.count === 5 && "Count is 5"
+                }
+                {
+                    this.state.flag && <p>Flag is true</p>
                 }
             </div>
         );
