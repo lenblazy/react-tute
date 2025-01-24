0b7dd3b9b29afdb4615ebe486b4483aef1019545
Lennox Mwabonje <lenblazy@gmail.com>
2025-01-24 10:26:30 +0300

    Implement useReducer

--- src/components/FunctionalBasedComponent.jsx
+++ src/components/FunctionalBasedComponent.jsx
@@ -1,7 +1,13 @@
-import React, {useContext} from "react";
+import React, {useContext, useReducer} from "react";
 import PropsExample from "../PropExample/index.jsx";
 import NewChild from "../PropExample/newChild.jsx";
 import {Context} from "../CounterApp.jsx";
+
+//useState
+//useEffect
+//useContext
+//useReducer
+//useRef
 
 const Child = () => {
     return (
@@ -9,7 +15,25 @@ const Child = () => {
     )
 }
 
+const initialState = {flag: false};
+
+const reducer = (state, action) => {
+    switch (action.type) {
+        case "TOGGLE_BUTTON":
+            console.log(state, action);
+            return {
+                ...state,
+                flag: !state.flag,
+            };
+        default:
+            return state;
+    }
+}
+
 function FunctionalBasedComponent() {
+
+    const [state, dispatch] = useReducer(reducer, initialState)
+    console.log(state);
 
     const parentComponentHandler = () => console.log("Hello from Parent");
 
@@ -22,8 +46,11 @@ function FunctionalBasedComponent() {
         <div>
             Hello from FunctionalBasedComponent
             <PropsExample parentComponentHandler={parentComponentHandler} flag={true} data="data as prop"/>
-            <NewChild getValueFromChildComponent={getValueFromChildComponent} />
+            <NewChild getValueFromChildComponent={getValueFromChildComponent}/>
+            <br /><br />
             <button style={{backgroundColor: getValueFromContext}}>Click me</button>
+            <br /><br />
+            <button onClick={() => dispatch({type: 'TOGGLE_BUTTON'})}>Toggle</button>
         </div>
     );
 }
